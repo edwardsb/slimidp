112b2636fb33764be0b14760d4a4dbb0a85e211e
Benjamin Edwards <edwards.benw@gmail.com>
2019-01-09 09:52:04 -0500

    solicited login working

--- routes/sso.js
+++ routes/sso.js
@@ -15,23 +15,75 @@ const SimpleProfileMapper = require('../lib/simpleProfileMapper.js');
  stage: https://nebula-retina-mb-stage.eng-prod.mb-internal.com/sso/acs/{some id we have yet to determine}
  prod:  https://cloud.malwarebytes.com/sso/acs/{some id we have yet to determine}
  */
-const assertionConsumerServiceURL = process.env.ACSURL || 'http://cloud.malwarebytes.localhost/sso/acs/60d191ca-9fe8-4202-a24f-12341d7e9541'; //default local environment
+const assertionConsumerServiceURL = process.env.ACSURL || 'http://cloud.malwarebytes.localhost/sso/acs/5f111e49-3d12-431a-8749-8ad1d3929ea3'; //default local environment
 
 /* GET users listing. */
 router.post('/', function (req, res) {
 
 	req.user = req.body;
 
-	// do the sso things
-	samlp.auth({
-		issuer:     'msp',
-		cert:       fs.readFileSync(path.join(__dirname, '../signing-cert-pub.pem')), // we will need to share this certificate
-		key:        fs.readFileSync(path.join(__dirname, '../signing-cert-pvt.key')),
+	const authOptions = {
+		issuer: 'oneview',
+		cert: fs.readFileSync(path.join(__dirname, '../signing-cert-pub.pem')), // we will need to share this certificate
+		key: fs.readFileSync(path.join(__dirname, '../signing-cert-pvt.key')),
 		getPostURL: function (wtrealm, wreply, req, callback) {
-			return callback( null, assertionConsumerServiceURL)
+			return callback(null, assertionConsumerServiceURL)
 		},
 		profileMapper: SimpleProfileMapper
-	})(req, res)
+	};
+
+	// handle
+	if (req.user._authnRequest) {
+		req.authnRequest = JSON.parse(req.user._authnRequest);
+
+		// Apply AuthnRequest Params
+		authOptions.inResponseTo = req.authnRequest.id;
+		authOptions.acsUrl = req.authnRequest.acsUrl;
+		authOptions.recipient = req.authnRequest.acsUrl;
+		authOptions.destination = req.authnRequest.acsUrl;
+		authOptions.forceAuthn = req.authnRequest.forceAuthn;
+		if (req.authnRequest.relayState) {
+			authOptions.RelayState = req.authnRequest.relayState;
+		}
+
+		authOptions.getPostURL = function (wtrealm, wreply, req, callback) {
+			return callback(null, req.authnRequest.acsUrl)
+		}
+	}
+
+	// do the sso things
+	samlp.auth(authOptions)(req, res)
 });
+
+const processServiceProviderInitiated = function (req, res) {
+	samlp.parseRequest(req, function (err, data) {
+		if (err) {
+			return res.render('error', {
+				message: 'SAML AuthnRequest Parse Error: ' + err.message,
+				error: err
+			})
+		}
+		if (data) {
+			req.authnRequest = {
+				relayState: req.query.RelayState || req.body.RelayState,
+				id: data.id,
+				issuer: data.issuer,
+				destination: data.destination,
+				acsUrl: data.assertionConsumerServiceURL,
+				forceAuthn: data.forceAuthn === 'true'
+			};
+			console.log('Received AuthnRequest => \n', req.authnRequest);
+
+			res.render('user', {
+				title: "Solicited Login",
+				authnRequest: req.authnRequest
+			})
+
+		}
+	})
+};
+
+router.get('/login', processServiceProviderInitiated);
+router.post('/login', processServiceProviderInitiated);
 
 module.exports = router;
